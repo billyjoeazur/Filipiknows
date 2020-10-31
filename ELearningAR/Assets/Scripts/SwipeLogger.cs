44df24ec2668a4cebfe11590432a985331d1742d
billyjoeazur <72329332+billyjoeazur@users.noreply.github.com>
2020-10-31 14:43:44 +0800

    Add Vuforia Database, Add Target Image, Add 3d Numbers Assets

---
+++ ELearningAR/Assets/Scripts/SwipeLogger.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+public class SwipeLogger : MonoBehaviour
+{
+
+	public GameObject[] numbers;
+	int i = 0;
+
+	private void Awake()
+    {
+        SwipeDetector.OnSwipe += SwipeDetector_OnSwipe;
+		numbers[0].SetActive(true);
+    }
+
+    private void SwipeDetector_OnSwipe(SwipeData data)
+    {
+		Debug.Log("Swipe in Direction: " + data.Direction);
+		if (i <= numbers.Length)
+		{
+			if (data.Direction.ToString() == "Left")
+			{
+				numbers[i].SetActive(false);
+				if (i == numbers.Length -1)
+				{
+					i = numbers.Length -1;
+				}
+				else
+				{
+					i++;
+				}
+			}
+
+			else if (data.Direction.ToString() == "Right")
+			{
+				numbers[i].SetActive(false);
+				
+				if (i == 0)
+				{
+					i = 0;
+				}
+				else
+				{
+					i--;
+				}
+			}
+			numbers[i].SetActive(true);
+
+			Debug.Log(numbers[i]);
+		}
+		
+	}
+
+}
